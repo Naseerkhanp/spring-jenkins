caeb7e316447374e31217be2963600c6d2d02a4c
Naseer Khan <naseerkhan200@gmail.com>
2022-07-14 18:03:23 +0530

    updated the pom

--- src/test/java/com/naseerkhan/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/naseerkhan/jenkins/api/SpringJenkinsApplicationTests.java
@@ -1,13 +1,20 @@
 package com.naseerkhan.jenkins.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
+	Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("Test case eecuting....");
+		assertEquals(true, true);
 	}
 
 }
